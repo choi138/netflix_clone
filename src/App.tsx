67c926456632ade8de50eb7e843a2a0155b259e4
Justin <kidjustinchoi@gmail.com>
2022-08-18 01:06:45 +0900

    type done

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,25 @@ const H1 = styled.h1`
   color: ${(props) => props.theme.textColor};
 `;
 
+interface DummyProps {
+  text: string;
+  active?: boolean; //끝에?를 했기 때문에 선택 가능한 interface임.
+}
+
+function Dummy({text, active = false}:DummyProps){ //active이 undefind라면 flase를 받는다.
+  return<H1>{text}</H1>
+}
+
 function App() {
+  const onClick = (event:React.MouseEvent<HTMLButtonElement>) => {
+    
+  }
   return(
     <Container>
-      <H1>protected</H1>
+      <Dummy active text="hello"/>                                                                                                                                                                                                                                                                                                                                                      
+        <button onClick={onClick}>Click me</button>
     </Container>
   )
 }
-
-
 
 export default App;

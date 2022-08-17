4b2e1d78c02efa2b3c2e9583b7f8adf580abd636
Justin <kidjustinchoi@gmail.com>
2022-08-17 23:07:38 +0900

    optinal props

--- src/Cricle.tsx
+++ src/Cricle.tsx
@@ -1,22 +1,34 @@
 import React from "react";
 import styled from "styled-components"
 
-interface CircleProps { //interface는 object의 타입을 설명해준다.
+interface ContainerProps{
     bgColor: string;
+    borderColor: string;
 }
 
-const Container = styled.div<CircleProps>`
+const Container = styled.div<ContainerProps>`
     width: 200px;
     height: 200px;
     background-color: ${(props) => props.bgColor};
     border-radius: 100px;
+    border: 1px solid ${(props) => props.borderColor};
 `;
 
-function Circle({bgColor}: CircleProps){
+interface CircleProps { //interface는 object의 타입을 설명해준다.
+    bgColor: string;  //끝에 ?를 안붙이면 얘는 필수가 된다.
+    borderColor?: string; // 필수가 아닌 옵션으로 바꾸는건 끝에 ?를 붙이면 된다.
+    text?: string;
+}
+
+function Circle({bgColor, borderColor, text = "defalut text"}: CircleProps){ //만약 text가 존재하지 않다면 defalut text 값을 넣어라
     return (
-    <Container bgColor={bgColor} />
+        <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+            {text}
+        </Container>
     );
 }
+
+// borderColor ?? bgColor 뜻은 만약 borderColor가 undefined상태라면 bgColor를 값으로 해라.
 
 export default Circle;
 

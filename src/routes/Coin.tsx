23b01b95da00eceb9373def19c189dd512e182c1
Justin <kidjustinchoi@gmail.com>
2022-08-31 22:17:54 +0900

    add style

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -26,6 +26,28 @@ const Title = styled.h1`
     font-size: 48px;
 `;
 
+const Overview = styled.div`
+   display: flex;
+   justify-content: space-between;
+   background-color: rgba(0, 0, 0, 0.5);
+   padding: 10px 20px;
+   border-radius: 10px;
+ `;
+ const OverviewItem = styled.div`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+
+   span:first-child {
+     font-size: 10px;
+     font-weight: 400;
+     text-transform: uppercase;
+     margin-bottom: 5px;
+   }
+ `;
+ const Description = styled.p`
+   margin: 20px 0px;
+ `;
 interface ILocation {
     state:{
     name:string;
@@ -103,23 +125,48 @@ function Coin(){
     useEffect(() => {
         (async () => {
             const infoData = await (
-                await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+                await fetch(`https://api.coinpaprika.com/v1/coins/`)
                 ).json();
             const priceData = await(
-                await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+                await fetch(`https://api.coinpaprika.com/v1/tickers`)
                 ).json();
             setInfo(infoData);
             setPriceInfo(priceData);
+            setLoading(false);
         })();
-    }, [])
+    }, [coinId])
     return(
         <Container>
             <Header>
-              <Title>{state.name || "Loading"}</Title>  
+              <Title>{state?.name ? state.name : loading ? "loading..." : info?.name}</Title>  
             </Header>
             {loading ? (
                 <Loader>Loading...</Loader>
-            ) : null}
+            ) : (
+                <>
+                    <Overview>
+                    <OverviewItem>
+                        <span>Rank:</span>
+                        <span>{info?.rank}</span>
+                    </OverviewItem>
+                    <OverviewItem>
+                        <span>Symbol:</span>
+                        <span>{info?.symbol}</span>
+                    </OverviewItem>
+                    <OverviewItem>
+                        <span>Open Source:</span>
+                        <span>{info?.open_source}</span>
+                    </OverviewItem>
+                    </Overview>
+                        <Description>{info?.description}</Description>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Max Supply:</span>
+                            <span>{priceInfo?.max_supply}</span>
+                        </OverviewItem>
+                    </Overview>
+                </>
+            )}
         </Container>
     )
 }

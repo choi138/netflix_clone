6cddf6dfb2a550aa922e01c17bb54cec938b3112
Justin <kidjustinchoi@gmail.com>
2022-09-06 23:33:02 +0900

    asdf

--- src/api.ts
+++ src/api.ts
@@ -2,11 +2,13 @@ const API_KEY = "3e354f52d83220768ad48a5e6ab5121b"
 const BASE_PATH = "https://api.themoviedb.org/3"
 
 interface IMovie {
-    id: number;
     backdrop_path: string;
     poster_path: string;
-    title: string;
-    overview: string;
+    genre_ids: [number];
+    id: number; //
+    release_date: string; // "2019-10-04"
+    title: string; // original_title
+    overview: string; // original_overview
 }
 
 export interface IGetMoviesResult {

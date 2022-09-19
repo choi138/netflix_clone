86d60e0b4970d11a961218fd9716bad1b8396a05
최근원 <kidjustinchoi@gmail.com>
2022-09-19 09:24:53 +0900

    little change header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
+import { Movie } from "@mui/icons-material";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import {createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
 import Header from "./Components/Header/Header";
 import Modal from "./Components/Modal/Modal";
 import Home from "./Routes/Home/Home";
@@ -11,16 +12,17 @@ const GlobalStyle = createGlobalStyle`
 
 
 function App() {
-  return(
+  return (
     <Router>
-        <Header/>
-        <Routes>
-            {/* <Route path="/:part/:sliderPart/:id" element={<Tv/>}/>  TV프로그램들을 위한 라우트 */}
-            <Route path="/search" element={<Search/>}/>  {/*검색을 위한 라우트*/}
-            <Route path="/:part/:sliderPart/:id" element={<Home/>}/>  
-            {/* 영화들을 보여줄 home라우트 */}
-            <Route index element={<Home/>}/>
-        </Routes>
+      <Header />
+      <Routes>
+        {/* <Route path="/:part/:sliderPart/:id" element={<Tv/>}/>  TV프로그램들을 위한 라우트 */}
+        <Route path="/search" element={<Search />} />  {/*검색을 위한 라우트*/}
+        <Route path="/:part/:sliderPart/:id" element={<Home />} />
+        {/* 영화들을 보여줄 home라우트 */}
+        <Route index element={<Home />} />
+        <Route path="*" element={<div>404 Not Found</div>} />
+      </Routes>
     </Router>
   )
 }
